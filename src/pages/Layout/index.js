1f3b5bb7eb659c46e7958ad4e10ab5caa5c3d197
ywt <Yinwt100@qq.com>
2024-04-16 22:04:03 +0800

    feat: 完成主题色的定制化

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,12 @@
 import {Outlet} from 'react-router-dom'
+import {Button} from 'antd-mobile'
 const Layout = ()=>{
   return (
   <div>
     我的Layout
     <Outlet/>
+    {/* 测试全局生效样式 */}
+    <Button color='primary'>测试全局</Button>
   </div>
   )
 }
