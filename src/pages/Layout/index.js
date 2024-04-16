9a2c46d1034730936285dafbbf6aa7efc653bf2f
ywt <Yinwt100@qq.com>
2024-04-16 22:50:58 +0800

    feat: tabBar切换功能

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,22 +1,60 @@
-import {Outlet} from 'react-router-dom'
-import {Button} from 'antd-mobile'
-import { useEffect } from 'react'
-import {getBillList} from '@/store/modules/billStore'
-import {useDispatch} from 'react-redux'
+import { TabBar } from "antd-mobile"
+import { useEffect } from "react"
+import { Outlet } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { getBillList } from "@/store/modules/billStore"
+import './index.scss'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
+import {useNavigate} from 'react-router-dom'
 
-const Layout = ()=>{
-  const dispatch = useDispatch();
-  useEffect(()=>{
+
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
+
+const Layout = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
     dispatch(getBillList())
-  },[dispatch])
+  }, [dispatch])
+  // 切换菜单跳转路由
+  const navigate = useNavigate();
+  const switchRoute=(path)=>{
+    // console.log(path);
+    navigate(path)
+  }
   return (
-  <div>
-    我的Layout
-    <Outlet/>
-    {/* 测试全局生效样式 */}
-    <Button color='primary'>测试全局</Button>
-  </div>
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar  onChange={switchRoute}>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
+    </div>
   )
 }
 
-export default Layout;
+export default Layout
