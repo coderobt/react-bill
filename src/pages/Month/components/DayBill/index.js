26a643b7de704e62b910badbdf111d22c94ddc76
ywt <Yinwt100@qq.com>
2024-04-17 15:26:53 +0800

    feat: 点击切换展开和收起状态

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import './index.scss'
 import { useMemo } from 'react'
 import {billTypeToName} from '@/contants'
+import { useState } from 'react'
 
 const DailyBill = ({date,billList}) => {
   const dayResult = useMemo(()=>{
@@ -14,12 +15,16 @@ const DailyBill = ({date,billList}) => {
       total:pay+income
     }
   },[billList])
+
+  //控制展开收起
+  const [visible,setVisible] = useState(false)
   return (
     <div className={classNames('dailyBill')}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames('arrow')}></span>
+          {/* expand 有这个类名 展开的箭头朝上的样子 */}
+          <span onClick={()=>setVisible(!visible)} className={classNames('arrow',visible && 'expand')}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -37,7 +42,7 @@ const DailyBill = ({date,billList}) => {
         </div>
       </div>
       {/* 单日列表 */}
-      <div className="billList">
+      <div className="billList" style={{display:visible?'block':'none'}}>
         {billList.map(item => {
           return (
             <div className="bill" key={item.id}>
