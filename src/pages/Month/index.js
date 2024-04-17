4c6fd676b19b59feb6eb6e7ab1f2d4f3cb7d3fc8
ywt <Yinwt100@qq.com>
2024-04-17 13:28:03 +0800

    feat: 月度-账单-切换时间显示

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,7 +1,23 @@
 import { NavBar, DatePicker } from 'antd-mobile'
+import { useState } from 'react'
 import './index.scss'
+import classNames from 'classnames'
+import dayjs from 'dayjs'
 
 const Month = () => {
+  // 控制弹框的打开和关闭
+  const [dateVisible, setDateVisible] = useState(false)
+
+  //控制时间显示
+  const [currentDate,setCurrentDate]=useState(()=>{
+    return dayjs(new Date()).format('YYYY-MM')
+  })
+
+  const onConfirm=(date)=>{
+    setDateVisible(false)
+    setCurrentDate(dayjs(date).format('YYYY-MM'))
+  }
+
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -10,11 +26,12 @@ const Month = () => {
       <div className="content">
         <div className="header">
           {/* 时间切换区域 */}
-          <div className="date">
+          <div onClick={()=>setDateVisible(true)} className="date">
             <span className="text">
-              2023 | 3月账单
+              {currentDate}月账单
             </span>
-            <span className='arrow expand'></span>
+            {/* expand箭头朝上 没有朝下 */}
+            <span  className={classNames('arrow',dateVisible && 'expand')}></span>
           </div>
           {/* 统计区域 */}
           <div className='twoLineOverview'>
@@ -36,8 +53,11 @@ const Month = () => {
             className="kaDate"
             title="记账日期"
             precision="month"
-            visible={false}
+            visible={dateVisible}
             max={new Date()}
+            onCancel={()=>setDateVisible(false)}
+            onConfirm={onConfirm}
+            onClose={()=>setDateVisible(false)}
           />
         </div>
       </div>
